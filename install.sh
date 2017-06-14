a97369ef204362fcb260af9a2104c7dc7ad2ab2c
Aaron McConnell <amcconnell@duosecurity.com>
2017-06-14 16:36:42 -0400

    Updating Duo Web SDK to version 2.6.  Also changing /preauth API call to /check API call.

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ JIRA=/opt/atlassian/jira
 AKEY=`python -c "import hashlib, os;  print hashlib.sha1(os.urandom(32)).hexdigest()"`
 
 # duo file variables
-DUO_WEB_FILENAME=DuoWeb-1.2-SNAPSHOT.jar
+DUO_WEB_FILENAME=DuoWeb-1.3.jar
 DUO_CLIENT_FILENAME=duo-client-0.2.1.jar
-DUO_FILTER_FILENAME=duo-filter-1.3.6-SNAPSHOT.jar
-DUO_PLUGIN_FILENAME=duo-twofactor-1.4.1-SNAPSHOT.jar
+DUO_FILTER_FILENAME=duo-filter-1.3.7.jar
+DUO_PLUGIN_FILENAME=duo-twofactor-1.4.2.jar
 
 usage () {
     printf >&2 "Usage: $0 [-d JIRA directory] -i ikey -s skey -h host\n"
